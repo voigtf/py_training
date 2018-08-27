792ea24912c0d8542b7c4d9a400542ef64af152c
voigtf <41285173+voigtf@users.noreply.github.com>
2018-08-27 23:49:35 +0200

    5 diff books

--- lec9/test_potter_kata.py
+++ lec9/test_potter_kata.py
@@ -1,6 +1,6 @@
 # 24/08/2018 - https://www.codingdojo.org/kata/Potter/
 
-from Py_training.lec9.potter_kata import *
+from lec9.potter_kata import *
 
 # data
 # price = None  # type: float
@@ -61,3 +61,48 @@ def test_basket_countTotalPrice_buyTwoDiffBooks_fiveProcentOfDiscount_ut():
     basket.add_book(second_book)
 
     assert basket.count_total_price() == 15.2     # 15.2 = (0.95 * 16.0)
+
+
+def test_basket_countTotalPrice_buyThreeDiffBooks_tenProcentOfDiscount_ut():
+    first_book = Book('Potter1', 8.0)
+    second_book = Book('Potter2', 8.0)
+    third_book = Book('Potter3', 8.0)
+
+    basket = Basket()
+    basket.add_book(first_book)
+    basket.add_book(second_book)
+    basket.add_book(third_book)
+
+    assert basket.count_total_price() == 21.6     # 21.6 = (0.90 * 24.0)
+
+
+def test_basket_countTotalPrice_buyFourDiffBooks_twentyProcentOfDiscount_ut():
+    first_book = Book('Potter1', 8.0)
+    second_book = Book('Potter2', 8.0)
+    third_book = Book('Potter3', 8.0)
+    fourth_book = Book('Potter4', 8.0)
+
+    basket = Basket()
+    basket.add_book(first_book)
+    basket.add_book(second_book)
+    basket.add_book(third_book)
+    basket.add_book(fourth_book)
+
+    assert basket.count_total_price() == 25.6     # 25.6 = (0.80 * 32.0)
+
+
+def test_basket_countTotalPrice_buyFiveDiffBooks_twentyFiveProcentOfDiscount_ut():
+    first_book = Book('Potter1', 8.0)
+    second_book = Book('Potter2', 8.0)
+    third_book = Book('Potter3', 8.0)
+    fourth_book = Book('Potter4', 8.0)
+    fifth_book = Book('Potter5', 8.0)
+
+    basket = Basket()
+    basket.add_book(first_book)
+    basket.add_book(second_book)
+    basket.add_book(third_book)
+    basket.add_book(fourth_book)
+    basket.add_book(fifth_book)
+
+    assert basket.count_total_price() == 30.0     # 30.0 = (0.75 * 40)
